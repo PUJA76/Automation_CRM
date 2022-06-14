82ac5fa74acb4dac3e089b54ae8d3cebfd77072f
Puja Bohara <pujabohara507@gmail.com>
2022-06-14 13:26:20 +0545

    Notification_reply,Birthdate

--- Backend.py
+++ Backend.py
@@ -10,6 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 # Third Approach
+#Login
 def open_url():
     s = Service(ChromeDriverManager().install()) # This will install driver automatically on the runtime and save it in the cache
     driver = webdriver.Chrome(service = s)
@@ -31,14 +32,25 @@ def open_url():
     time.sleep(3)
     #driver.quit()
 
+#Notification_Message module
     notification_message = driver.find_element(By.XPATH, '//input[@id="Message"]')
     notification_message.send_keys("Hello")
     time.sleep(2)
 
+#Message & Reply module
     Message = driver.find_element(By.XPATH, '//a[contains(text(),"Message")]')
     Message.click()
     time.sleep(2)
+    Reply = driver.find_element(By.XPATH, '//a[contains(text(),"Reply")]')
+    Reply.click()
+    Type_reply = driver.find_element(By.XPATH, '//textarea[@id="txtReply"]')
+    Type_reply.send_keys("Hello")
+    time.sleep(2)
+    Send = driver.find_element(By.XPATH, '//input[@id="btnSend"]')
+    Send.click()
+    time.sleep(2)
 
+#Notification module
     Notifications = driver.find_element(By.XPATH, '//a[contains(text(),"Notifications")]')
     Notifications.click()
     time.sleep(2)
@@ -57,24 +69,20 @@ def open_url():
     Mobile_No = driver.find_element(By.XPATH, '//input[@id="MobileNo"]')
     Mobile_No.send_keys("9814542883")
     time.sleep(2)
+    Member_Birthday = driver.find_element(By.Xpath, '//input[@id="dtBFrom"]')
+    Member_Birthday.click()
+    setdatestring = "12/08/2008" #dd/mm/yy
 
-
-
+    time.sleep(2)
+    Submit = driver.find_element(By.XPATH, '//input[@type="submit"]')
+    Submit.click()  # click() --> call click action
+    time.sleep(3)
 
 
 if __name__ == '__main__':
      open_url()
 
 
-
-
-
-
-
-
-
-
-# Important Note
 """
 text() --> pull the text 
 select() --> Select from dropdown
